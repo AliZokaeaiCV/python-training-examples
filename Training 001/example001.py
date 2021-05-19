dbf2e5e69cb44f7eb931b72f4d11d70efdc97644
AliZokaeai <ali.zokaei.gm@gmail.com>
2021-05-19 13:34:39 +0430

    Second exercise

--- Training 001/example001.py
+++ Training 001/example001.py
@@ -12,3 +12,19 @@ sum = num1 + num2
 # printing values
 
 print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+# Python3 program to add two numbers
+
+number_1 = input("Import your first number: ")
+number_2 = input("\n Import your second number: ")
+
+
+# Adding two numbers
+# User might alos enter float numbers
+
+Sum_two = float(number_1) + float(number_2)
+
+# Display the sum
+# Will print value in float
+
+print("The sum of {0} and {1} is {2}". format(number_1, number_2, Sum_two))
