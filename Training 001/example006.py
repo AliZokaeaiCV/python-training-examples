4e7970888b329b47fbcaffe2975646455ca031d3
AliZokaeai <ali.zokaei.gm@gmail.com>
2021-05-20 18:16:51 +0430

    the sixth exercise: Armstrong Number - edit

--- Training 001/example006.py
+++ Training 001/example006.py
@@ -39,7 +39,7 @@ def power(x, y):
     if y == 0:
         return 1
     if y % 2 == 0:
-        return power(x, y // 2)
+        return power(x, y // 2) * power(x, y // 2)
 
     return x * power(x, y // 2) * power(x, y // 2)
 
@@ -52,6 +52,7 @@ def order(x):
     while (x != 0):
         n = n + 1
         x = x // 10
+
     return n
 
 
@@ -66,18 +67,15 @@ def isArmstrong(x):
 
     while (temp != 0):
         r = temp % 10
-
-
         sum1 = sum1 + power(r, n)
-
         temp = temp // 10
+
     # If condition satisfies
     return (sum1 == x)
 
 
 def list_of_Armsroungs(x):
     list_of_numebrs = []
-    zer = 0
 
     while (x != 0):
         if isArmstrong(x):
@@ -86,11 +84,12 @@ def list_of_Armsroungs(x):
         else:
             x = x - 1
             continue
+
     return list_of_numebrs
 
 
 # Driver code
 x = int(input("Please enter your number: "))
-Armstrong = str(list_of_Armsroungs(x))
+
 print("You Armstrong numbers between {0} to {1} is/are:".format(0, x))
-print(Armstrong)
+print(list_of_Armsroungs(x))
